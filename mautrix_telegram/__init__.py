7e46b098a67937f502602e02d16355f6a0c0e78a
Arcady Chumachenko <arcady.chumachenko@gmail.com>
2021-11-12 13:10:57 +0000

    Cleanup

--- mautrix_telegram/__init__.py
+++
@@ -1,2 +0,0 @@
-__version__ = "0.10.0"
-__author__ = "Tulir Asokan <tulir@maunium.net>"
--- mautrix_telegram/commands/__init__.py
+++
@@ -1,8 +0,0 @@
-from .handler import (command_handler, CommandHandler, CommandProcessor, CommandEvent,
-                      SECTION_AUTH, SECTION_CREATING_PORTALS, SECTION_PORTAL_MANAGEMENT,
-                      SECTION_MISC, SECTION_ADMIN)
-from . import portal, telegram, matrix_auth, manhole
-
-__all__ = ["command_handler", "CommandHandler", "CommandProcessor", "CommandEvent",
-           "SECTION_AUTH", "SECTION_MISC", "SECTION_ADMIN", "SECTION_CREATING_PORTALS",
-           "SECTION_PORTAL_MANAGEMENT"]
